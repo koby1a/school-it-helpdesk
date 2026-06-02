c1e26794bae79e4586e1b4133c33029ee6d947e2
TMateusz Kobylarczyk <kobylarczykmateusz8@gmail.com>
2026-06-02 20:57:05 +0200

    Initialize Spring Boot Maven project

---
+++ src/main/java/pl/school/ithelpdesk/IthelpdeskApplication.java
@@ -0,0 +1,13 @@
+package pl.school.ithelpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IthelpdeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IthelpdeskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/school/ithelpdesk/IthelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.school.ithelpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IthelpdeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
